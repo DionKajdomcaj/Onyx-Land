bafcac9894bbc13880357c61d4a22fd000616afe
Kajdomcaj Dion <e9tz67@inf.elte.hu>
2021-05-07 15:26:50 +0000

    Upload New File

---
+++ tests/Buildings/PathTest.java
@@ -0,0 +1,15 @@
+package Buildings;
+
+import Point.Point;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class PathTest {
+    @Test
+    public void draw() {
+
+    }
+}
