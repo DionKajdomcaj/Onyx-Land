3d5f51655d1269979562db7cfcf493743f944c62
Kajdomcaj Dion <e9tz67@inf.elte.hu>
2021-05-07 15:26:26 +0000

    Upload New File

---
+++ tests/Buildings/BuildingsTest.java
@@ -0,0 +1,44 @@
+package Buildings;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BuildingsTest {
+
+    @Test
+    public void getInfo() {
+    }
+
+    @Test
+    public void upgradeTheLevel() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void built() {
+    }
+
+    @Test
+    public void repair() {
+    }
+
+    @Test
+    public void clean() {
+    }
+
+    @Test
+    public void collides() {
+    }
+
+    @Test
+    public void draw() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+}
