da76a2148c97c780e5ecf01a37210ed7c80023d1
Kajdomcaj Dion <e9tz67@inf.elte.hu>
2021-05-07 15:24:20 +0000

    Upload New File

---
+++ tests/MouseInputTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MouseInputTest {
+
+    @Test
+    public void mouseClicked() {
+    }
+
+    @Test
+    public void mouseEntered() {
+    }
+
+    @Test
+    public void mouseExited() {
+    }
+
+    @Test
+    public void mousePressed() {
+    }
+
+    @Test
+    public void mouseReleased() {
+    }
+}
